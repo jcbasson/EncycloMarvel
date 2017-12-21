9d14b54ef0155d453c2a9a624ab46fcc156e7e22
JCBasson <jan.basson@gmail.com>
2017-12-21 23:24:26 +0200

    Connected the state with the marvelCharacterListComponent to display just 20 record for now while I figured out the scroll filter functionality.

--- client/components/marvelCharactersList/marvelCharacterListComponent.js
+++ client/components/marvelCharactersList/marvelCharacterListComponent.js
@@ -1,7 +1,15 @@
 import {connect} from 'react-redux';
 import MainContentContainer from './view/marvelCharactersListContainer';
 
+const mapStateToProps = (state) => {
+    const {MiningMarvelCharactersReducer} = state;
+    let {isFirstBatchOfCharactersRetrieved, marvelCharacters} = MiningMarvelCharactersReducer;
+    marvelCharacters = marvelCharacters.slice(0, 20);
+    return {isFirstBatchOfCharactersRetrieved, marvelCharacters};
+};
+
 const MarvelCharactersListComponent = connect(
+    mapStateToProps
 )(MainContentContainer);
 
 export default MarvelCharactersListComponent;
--- client/components/marvelCharactersList/view/marvelCharactersListPresenter.js
+++ client/components/marvelCharactersList/view/marvelCharactersListPresenter.js
@@ -5,34 +5,14 @@ import MarvelCharacterListItemComponent from '../../marvelCharacterListItem/marv
  * @Function MarvelCharactersListPresenter
  * @desc React Presenter function returning component jsx
  */
-const MarvelCharactersListPresenter = () => (
+const MarvelCharactersListPresenter = ({isFirstBatchOfCharactersRetrieved, marvelCharacters}) => (
     <section id="marvel-characters-list-component">
         <header className="marvel_characters_list_component__header"><h2>Marvel Characters A-Z</h2></header>
         <ul className="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
-            <MarvelCharacterListItemComponent></MarvelCharacterListItemComponent>
+            {!isFirstBatchOfCharactersRetrieved && <span>Busy Loading...</span>}
+            {isFirstBatchOfCharactersRetrieved  && marvelCharacters.map(function(marvelCharacter){
+                return <MarvelCharacterListItemComponent marvelCharacterName={marvelCharacter.name} marvelCharacterImageUrl={marvelCharacter.thumbnail.path} marvelCharacterImageExtension={marvelCharacter.thumbnail.extension}></MarvelCharacterListItemComponent>;
+            })}
         </ul>
     </section>
 );
